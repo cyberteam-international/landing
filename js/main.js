f8eab94fc2b10cada95e588d8db0e17128b50412
Boffeer <41575886+Boffeer@users.noreply.github.com>
2023-09-05 09:53:23 +0300

    делает десктоп горизонтальный gsap scroll

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@
         $(this).css('background-image', 'url(' + bg + ')');
     });
 
-    
+
 
     /*------------------
 		Navigation
@@ -50,7 +50,7 @@
     });
 
 
-    
+
 
     /*------------------
         Video Popup
@@ -66,12 +66,12 @@
         loop: false,
         slidesPerView: 4,
         spaceBetween: 30,
-      
+
         // If we need pagination
         pagination: {
           el: '.swiper-pagination',
         },
-      
+
         // Navigation arrows
         navigation: {
           nextEl: '.swiper-button-next',
@@ -96,5 +96,49 @@
             }
           }
     });
+
+
+		/**
+		 *
+		 * Services carousel
+		 *
+		 */
+		gsap.registerPlugin(ScrollTrigger);
+
+		const servicesList = document.querySelector('.services__list');
+
+		function getServicesListScrollTriggerEnd(lengthModifier = 0) {
+			const SERVICES_CARDS = [...servicesList.querySelectorAll('.services__item')];
+			if (SERVICES_CARDS.length === 0) {
+				console.warn('No such cards');
+				return 0;
+			}
+
+			return SERVICES_CARDS[0].getBoundingClientRect().width * (SERVICES_CARDS.length - lengthModifier);
+		}
+
+		function getServicesFromOffsetX() {
+			const SERVICES_CARD = servicesList.querySelector('.services__item');
+			const OFFSET_CARDS_COUNT = 2;
+
+			return OFFSET_CARDS_COUNT * SERVICES_CARD.getBoundingClientRect().width
+		}
+
+		function getSrevicesToOffsetX() {
+			return 0 - (getServicesListScrollTriggerEnd(-2));
+		}
+
+		const servicesCardsTimeline = gsap.timeline({
+			scrollTrigger: {
+				// markers: true, // маркеры для отладки старта и конца
+				trigger: servicesList, // Установите триггером ваш контейнер галереи
+				scrub: true, // Разрешите pin scrub
+				pin: true, // Закрепите элемент в начале контейнера
+				start: "top-=200 top", // Начало анимации pin scrub
+				end: `bottom+=${getServicesListScrollTriggerEnd()} bottom`, // Конец анимации pin scrub, основанный на ширине контейнера
+			}
+		});
+
+		servicesCardsTimeline.fromTo(servicesList, {x: getServicesFromOffsetX}, {x: getSrevicesToOffsetX})
 
 })(jQuery);
